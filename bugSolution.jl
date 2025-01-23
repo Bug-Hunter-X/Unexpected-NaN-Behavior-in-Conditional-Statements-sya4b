b0f40652e33516de5619fcb4390c59fea0981c6f
BugHunter-X <askzenai@gmail.com>
2025-01-23 05:06:06 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,15 @@
+```julia
+function my_function(x)
+  if isnan(x)
+    return NaN  # Explicitly handle NaN
+  elseif x > 0
+    return x^2
+  else
+    return -x
+  end
+end
+
+println(my_function(2))   # Output: 4
+println(my_function(-2))  # Output: 2
+println(my_function(NaN)) # Output: NaN
+```
